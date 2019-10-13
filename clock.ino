68b3060b8ddba99e68bcbcba8e9caa1cdc4fcec5
Osbert Ngok <osbert.ngok@mujin.co.jp>
2019-10-13 18:59:59 +0900

    refactor for better structure

--- clock.ino
+++ clock.ino
@@ -2,18 +2,20 @@
 #include <LiquidCrystal_I2C.h>
 
 const char ZERO_CHAR = '0';
-const char STATE_ZERO_OFF = 0;
-const char STATE_ONE_ON = 1;
-const char STATE_ONE_OFF = 2;
-const char STATE_TWO_ON = 3;
-const char STATE_TWO_OFF = 4;
-const char STATE_THREE_ON = 5;
-const char STATE_THREE_OFF = 6;
-const char STATE_ONE_LONG = 7;
-const char STATE_TWO_LONG = 8;
-const char STATE_THREE_LONG = 9;
+
+const char STATE_ZERO_OFF = 0; // currently Off, push count 0
+const char STATE_ONE_ON = 1; // currently On, push count 1
+const char STATE_ONE_OFF = 2; // currently Off, push count 1
+const char STATE_TWO_ON = 3;  // currently On, push count 2
+const char STATE_TWO_OFF = 4;  // currently Off, push count 2
+const char STATE_THREE_ON = 5;  // currently On, push count 3
+const char STATE_THREE_OFF = 6;  // currently Off, push count 3
+const char STATE_ONE_LONG = 7; // currently holding, push count 1
+const char STATE_TWO_LONG = 8; // currently holding, push count 2
+const char STATE_THREE_LONG = 9; // currently holding, push count 3 (not used)
 
 const int keyPin = 7;
+const int STATE_SWITCH_THRESHOLD = 300;
 
 unsigned long base = 0;
 unsigned long lastEventTime = 0;
@@ -27,6 +29,7 @@ unsigned char hour, minute, second;
 LiquidCrystal_I2C lcd(0x27, 16, 2);
 
 void updateMemory(unsigned long diff) {
+  // make sure using unsigned long; 60 * 60 * 1000 would overflow)
   hour = diff / (60UL * 60UL * 1000UL) % 24;
   minute = diff / (60UL * 1000UL) % 60;
   second = diff / 1000UL % 60;
@@ -38,43 +41,71 @@ void updateMemory(unsigned long diff) {
   memory[7] = ZERO_CHAR + second % 10;
 }
 
-boolean process(unsigned long nowTime, char state) {
-  boolean processed = false;
+void ioChangeProcess(unsigned long nowTime, char state) {
+  switch (currentState) {
+    case STATE_ZERO_OFF:
+      currentState = STATE_ONE_ON;
+      lastEventTime = nowTime;
+      break;
+    case STATE_ONE_ON:
+      currentState = STATE_ONE_OFF;
+      lastEventTime = nowTime;
+      break;
+    case STATE_ONE_OFF:
+      currentState = STATE_TWO_ON;
+      lastEventTime = nowTime;
+      break;
+    case STATE_TWO_ON:
+      currentState = STATE_TWO_OFF;
+      lastEventTime = nowTime;
+      break;
+    case STATE_TWO_OFF:
+      currentState = STATE_THREE_ON;
+      lastEventTime = nowTime;
+      break;
+    case STATE_THREE_ON:
+      currentState = STATE_THREE_OFF;
+      lastEventTime = nowTime;
+      break;
+    case STATE_ONE_LONG:
+    case STATE_TWO_LONG:
+    case STATE_THREE_LONG:
+      currentState = STATE_ZERO_OFF;
+      lastEventTime = nowTime;
+      break;
+  }
+}
+
+void timeBasedProcess(unsigned long nowTime, char state) {
   switch (state) {
     case STATE_ONE_ON:
     case STATE_ONE_LONG:
       currentState = STATE_ONE_LONG;
       base -= 60UL * 60UL * 1000UL;
-      processed = true;
       lastEventTime = nowTime;
       break;
     case STATE_TWO_ON:
     case STATE_TWO_LONG:
       currentState = STATE_TWO_LONG;
       base -= 5UL * 60UL * 1000UL;
-      processed = true;
       lastEventTime = nowTime;
       break;
     case STATE_THREE_ON:
-      processed = true;
       lastEventTime = nowTime;
       break;
     case STATE_ONE_OFF:
       currentState = STATE_ZERO_OFF;
       base -= 60UL * 60UL * 1000UL;
-      processed = true;
       lastEventTime = nowTime;
       break;
     case STATE_TWO_OFF:
       currentState = STATE_ZERO_OFF;
       base -= 5UL * 60UL * 1000UL;
-      processed = true;
       lastEventTime = nowTime;
       break;
     case STATE_THREE_OFF:
       currentState = STATE_ZERO_OFF;
       base = millis();
-      processed = true;
       lastEventTime = nowTime;
       break;
   }
@@ -85,57 +116,19 @@ void setup()
 {
   lcd.init();
   lcd.backlight();
-  Serial.begin(9600);
 }
 
 void loop()
 {
-  Serial.print(currentState);
   unsigned long nowTime = millis();
   
-  // Don't response for the first second
-  if (nowTime > 3000) {
-    currentKey = digitalRead(keyPin);
-    if (currentKey != lastKey) {
-      switch (currentState) {
-        case STATE_ZERO_OFF:
-          currentState = STATE_ONE_ON;
-          lastEventTime = nowTime;
-          break;
-        case STATE_ONE_ON:
-          currentState = STATE_ONE_OFF;
-          lastEventTime = nowTime;
-          break;
-        case STATE_ONE_OFF:
-          currentState = STATE_TWO_ON;
-          lastEventTime = nowTime;
-          break;
-        case STATE_TWO_ON:
-          currentState = STATE_TWO_OFF;
-          lastEventTime = nowTime;
-          break;
-        case STATE_TWO_OFF:
-          currentState = STATE_THREE_ON;
-          lastEventTime = nowTime;
-          break;
-        case STATE_THREE_ON:
-          currentState = STATE_THREE_OFF;
-          lastEventTime = nowTime;
-          break;
-        case STATE_ONE_LONG:
-        case STATE_TWO_LONG:
-        case STATE_THREE_LONG:
-          currentState = STATE_ZERO_OFF;
-          lastEventTime = nowTime;
-          break;
-      }
-    } else {
-      if (nowTime - lastEventTime >= 300 || currentState == STATE_THREE_OFF) {
-        process(nowTime, currentState);
-      }
-    }
-    lastKey = currentKey;
+  currentKey = digitalRead(keyPin);
+  if (currentKey != lastKey) {
+    ioChangeProcess(nowTime, state);
+  } else if (nowTime - lastEventTime >= STATE_SWITCH_THRESHOLD || currentState == STATE_THREE_OFF) {
+    timeBasedProcess(nowTime, currentState);
   }
+  lastKey = currentKey;
   
   updateMemory(nowTime - base);
   lcd.setCursor(0, 0);
